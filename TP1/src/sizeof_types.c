a762558ffc3d9a5c6e901b1e649be595fb8ae918
weam95 <william.lambert.auditeur@lecnam.net>
2025-12-17 19:41:23 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    printf("Tailles des types de base en octets:\n");
+    printf("char: %zu\n", sizeof(char));
+    printf("short: %zu\n", sizeof(short));
+    printf("int: %zu\n", sizeof(int));
+    printf("long int: %zu\n", sizeof(long int));
+    printf("long long int: %zu\n", sizeof(long long int));
+    printf("float: %zu\n", sizeof(float));
+    printf("double: %zu\n", sizeof(double));
+    printf("long double: %zu\n", sizeof(long double));
+    return 0;
+}
