1b76db8aaa25778872e96bb1ca0283ff87a104ab
weam95 <william.lambert.auditeur@lecnam.net>
2025-12-17 20:06:04 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(void) {
+    int n = 5;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        
+        printf("\n");
+    }
+    
+    printf("\nGénération de la pyramide terminée.\n");
+    
+    return 0;
+}
