b26c874d242caf68d35b0e17e400dc5a056a5450
weam95 <william.lambert.auditeur@lecnam.net>
2025-12-17 20:02:34 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
 
+int main(void) {
+    int num1 = 20;
+    int num2 = 4;
+    char op = '+';
+    
+    printf("num1 = %d, num2 = %d, opérateur = '%c'\n\n", num1, num2, op);
+    
+    switch (op) {
+        case '+':
+            printf("Résultat : %d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("Résultat : %d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("Résultat : %d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Résultat : %d / %d = %d\n", num1, num2, num1 / num2);
+            } else {
+                printf("Erreur : division par zéro\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("Résultat : %d %% %d = %d\n", num1, num2, num1 % num2);
+            } else {
+                printf("Erreur : modulo par zéro\n");
+            }
+            break;
+        case '&':
+            printf("Résultat : %d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("Résultat : %d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("Résultat : ~%d = %d\n", num1, ~num1);
+            printf("Résultat : ~%d = %d\n", num2, ~num2);
+            break;
+        default:
+            printf("Opérateur non reconnu\n");
+            break;
+    }
+    
+    return 0;
+}
