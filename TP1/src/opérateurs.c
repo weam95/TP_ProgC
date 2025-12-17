d3a2dd8a6410d86bd847910ea5764365a6b77051
weam95 <william.lambert.auditeur@lecnam.net>
2025-12-17 19:53:46 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,19 @@
-
-
+#include <stdio.h>
+int main(void) {
+    int a = 16;
+    int b = 3;
+    
+    printf("a = %d, b = %d\n\n", a, b);
+    
+    printf("Addition : a + b = %d\n", a + b);
+    printf("Soustraction : a - b = %d\n", a - b);
+    printf("Multiplication : a * b = %d\n", a * b);
+    printf("Division : a / b = %d\n", a / b);
+    printf("Modulo : a %% b = %d\n", a % b);
+    printf("\n");
+    
+    printf("a est égal à b : %d\n", a == b);
+    printf("a est supérieur à b : %d\n", a > b);
+    
+    return 0;
+}
