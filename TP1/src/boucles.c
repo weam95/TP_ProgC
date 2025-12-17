aeb5ccb4446f420257e997cd43d12846bdfbc830
weam95 <william.lambert.auditeur@lecnam.net>
2025-12-17 19:55:39 +0100

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int compteur = 5;
+    
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
